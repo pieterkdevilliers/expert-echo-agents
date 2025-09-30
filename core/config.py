bd0539655809657deb63e344a4376ec0966ce009
Pieter K de Villiers <pieter@hey.com>
2025-09-30 11:04:06 +0100

    bfa-875 remove unused envs

--- core/config.py
+++ core/config.py
@@ -8,11 +8,6 @@ class Settings(BaseSettings):
     PROJECT_NAME: str = "ExpertEcho Agent Service"
     API_V1_PREFIX: str = "/api/v1"
     ENV: str = "development"
-    DATABASE_URL: str
-    AWS_SES_REGION: str
-    AWS_ACCESS_KEY: str
-    AWS_SECRET_KEY: str
-    AWS_SES_VERIFIED_MAIL: str
     OPENAI_API_KEY: str
     AI_API_KEY: str
     dev_key: str
